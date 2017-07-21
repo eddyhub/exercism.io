ecf9f32b86596f642d3e6a124c8682e36c3e0275
eddyhub <eddyhub@users.noreply.github.com>
2017-07-21 19:07:54 +0200

    Added second iteration of largest-series-product

--- java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
+++ java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -1,22 +1,26 @@
+import java.util.stream.IntStream;
+
 public class LargestSeriesProductCalculator {
 
-    String s;
+    String str;
 
     public LargestSeriesProductCalculator(String s) {
-        if(s == null) throw new IllegalArgumentException("String to search must be non-null.");
-        if(s.matches(".*\\D+.*")) throw new IllegalArgumentException("String to search may only contains digits.");
-        this.s = s;
+        if (s == null) throw new IllegalArgumentException("String to search must be non-null.");
+        if (s.matches(".*\\D+.*")) throw new IllegalArgumentException("String to search may only contains digits.");
+        this.str = s;
     }
 
     public long calculateLargestProductForSeriesLength(int n) {
-        if(n < 0) throw new IllegalArgumentException("Series length must be non-negative.");
-        if(s.length() < n) throw new IllegalArgumentException("Series length must be less than or equal to the length of the string to search.");
-        long max = 0;
-        for (int i = 0; i <= s.length() - n; i++) {
-            String substring = s.substring(i, i + n);
-            long tmp = substring.chars().map(Character::getNumericValue).mapToLong(Long::valueOf).reduce(1, (k,l) -> k * l);
-            if (max < tmp) max = tmp;
-        }
-        return max;
+        if (n < 0) throw new IllegalArgumentException("Series length must be non-negative.");
+        return IntStream
+                .rangeClosed(0, str.length() - n)
+                .mapToObj(index -> str.substring(index, index + n))
+                .mapToLong(s -> s
+                        .chars()
+                        .map(Character::getNumericValue)
+                        .mapToLong(Long::valueOf)
+                        .reduce(1, (k, l) -> k * l))
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException("Series length must be less than or equal to the length of the string to search."));
     }
 }
