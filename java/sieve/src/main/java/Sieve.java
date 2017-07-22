ccb269e7e314edfff8e643c5a37ddab67e3b7426
eddyhub <eddyhub@users.noreply.github.com>
2017-07-22 07:00:58 +0200

    Sieves with primitivs is much faster...

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -1,18 +1,25 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Sieve {
     int maxPrime;
-    List<Integer> numbers;
+    int[] primes;
 
     Sieve(int maxPrime) {
         this.maxPrime = maxPrime;
-        numbers = IntStream.rangeClosed(2, this.maxPrime).boxed().collect(Collectors.toList());
+        primes = IntStream.rangeClosed(2, maxPrime).toArray();
     }
 
     List<Integer> getPrimes() {
-        IntStream.rangeClosed(2, (int)Math.sqrt(maxPrime)).forEach(i -> numbers.removeIf(j -> j % i == 0 && j / i != 1));
-        return numbers;
+        for (int i = 2; i <= (int) Math.sqrt(maxPrime); i++) {
+            for (int j = 0; j < primes.length; j++) {
+                if (primes[j] != 0 && primes[j] % i == 0 && primes[j] / i != 1) {
+                    primes[j] = 0;
+                }
+            }
+        }
+        return Arrays.stream(primes).filter(i -> i != 0).boxed().collect(Collectors.toList());
     }
 }
