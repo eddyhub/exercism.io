2daf41069469d3f16a0b99be14f83a1ec57b4f71
eddyhub <eddyhub@users.noreply.github.com>
2017-07-25 10:05:41 +0200

    collatz-conjecture without recursion

--- java/collatz-conjecture/src/main/java/CollatzCalculator.java
+++ java/collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,16 +1,9 @@
-/**
- * Created by edi on 25.07.17.
- */
 public class CollatzCalculator {
-    int steps;
-
     public int computeStepCount(int i) {
         if (i < 1) throw new IllegalArgumentException("Only natural numbers are allowed");
-        
-        if (i != 1) {
-            steps++;
-            if (i % 2 == 0) computeStepCount(i / 2);
-            else computeStepCount(3 * i + 1);
+        int steps = 0;
+        for(; i != 1; steps++) {
+            i = i % 2 == 0 ? i / 2 : 3*i+1;
         }
         return steps;
     }
